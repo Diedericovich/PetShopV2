717861a8b295ef087c3a02a3c1cdff59d8eedae9
Kobe Delobelle <delobellekobe@gmail.com>
2021-04-21 09:03:36 +0200

    comments login

--- PetShopV2/PetShopV2/Views/AboutPage.xaml.cs
+++ PetShopV2/PetShopV2/Views/AboutPage.xaml.cs
@@ -1,12 +1,16 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using Xamarin.Forms;
 
 namespace PetShopV2.Views
 {
     public partial class AboutPage : ContentPage
     {
+
         public AboutPage()
         {
             InitializeComponent();
         }
+
+   
     }
 }
